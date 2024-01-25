19a68d84ba9d3d884e280e6e8918d827cde7643e
anneliis.poll <anneliis.poll@gmail.com>
2024-01-25 10:06:43 +0200

    first commit

---
+++ Crud-operations.js
@@ -0,0 +1,19 @@
+// create an array
+const flowers = ["sunflower", "daffodil", "daisy"];
+console.log(flowers);
+
+// update an array
+
+flowers.push("tulips", "roses", "lilies", "peonies");
+console.log(flowers);
+
+// delete last element from an array
+
+let deleteFlower = flowers.pop();
+console.log(deleteFlower);
+console.log(flowers);
+
+// read from an array
+
+let secondflower = flowers[1];
+console.log(secondflower);
